6ffd6520c5ab2142328d4df53cf04fe57a15a12a
Anonda25 <anondabiswas25@gmail.com>
2025-02-03 17:04:59 +0600

    added the resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import  { useState } from 'react'
 import { HiMenuAlt3, HiMenuAlt1 } from "react-icons/hi";
 import ResponsiveMenu from './ResponsiveMenu';
 import { Download } from 'lucide-react';
@@ -23,7 +23,7 @@ const Navbar = () => {
             <a href="#about"><li className='cursor-pointer'>About</li></a>
             <a href="#education"><li className='cursor-pointer'>Education & My Skills</li></a>
             <a href="#project"><li className='cursor-pointer'>Projects</li></a>
-            <a href="#" ><button className='px-3 py-1 cursor-pointer rounded-md bg-blue-500 text-white flex items-center gap-1 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#33CCCC,0_0_15px_#33CCCC,0_0_30px_#33CCCC] shadow-blue-500 border-blue-400 border-2'><Download className='w-5 h-5' />Resume Download</button></a>
+            <a target='_blank' href="/public/_Resume_of_Anonda Biswas (1).pdf" download={'/public/_Resume_of_Anonda Biswas (1).pdf'}  ><button  className='px-3 py-1 cursor-pointer rounded-md bg-blue-500 text-white flex items-center gap-1 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#33CCCC,0_0_15px_#33CCCC,0_0_30px_#33CCCC] shadow-blue-500 border-blue-400 border-2'><Download className='w-5 h-5' />Resume Download</button></a>
           </ul>
         </nav>
         <div className='md:hidden text-white text-4xl'>
